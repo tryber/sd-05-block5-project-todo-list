dbdcf213ee922f1bd5dd6350b0fda74720f6fa2c
Felipe Vieira <flpvieira86@gmail.com>
2020-05-23 20:21:30 -0300

    adicionada função que troca cor de fundo da tarefa quando clicada

--- script.js
+++ script.js
@@ -2,14 +2,25 @@
 const ol = document.getElementById('lista-tarefas');
 const addTask = document.getElementById('criar-tarefa');
 const taskText = document.getElementById('texto-tarefa');
-
+let target;
 // funcoes
 function adicionarTarefa() {
   const li = document.createElement('li');
   ol.appendChild(li);
   li.innerText = taskText.value;
+  li.className = 'task';
   taskText.value = '';
 }
 
+function trocaBg() {
+  target = event.target;
+  console.log(event.target);
+  if (target.classList.length === 1) {
+    target.classList.add('grayBg');
+  } else {
+    target.classList.remove('grayBg');
+  }
+}
 // eventListeners
 addTask.addEventListener('click', adicionarTarefa);
+ol.addEventListener('click', trocaBg);
