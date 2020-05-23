d9eaa5fc5d7a7e0f4c3a79f89f7142a19de3d888
Felipe Vieira <flpvieira86@gmail.com>
2020-05-23 19:52:22 -0300

    adicionada funcao que adiciona a tarfa na lista ordenada

---
+++ script.js
@@ -0,0 +1,15 @@
+// variaveis
+const ol = document.getElementById('lista-tarefas');
+const addTask = document.getElementById('criar-tarefa');
+const taskText = document.getElementById('texto-tarefa');
+
+
+// funcoes
+function adicionarTarefa() {
+  const li = document.createElement('li');
+  ol.appendChild(li);
+  li.innerText = taskText.value;
+}
+
+// eventListeners
+addTask.addEventListener('click', adicionarTarefa);
