5dd8e6123c99ce6e6a380e5d4cae8524c1d0e07f
Felipe Vieira <flpvieira86@gmail.com>
2020-05-23 19:56:53 -0300

    faltava função de limpar o input apos clicar no botão de adicinar. não falta mais !

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const ol = document.getElementById('lista-tarefas');
 const addTask = document.getElementById('criar-tarefa');
 const taskText = document.getElementById('texto-tarefa');
 
-
 // funcoes
 function adicionarTarefa() {
   const li = document.createElement('li');
   ol.appendChild(li);
   li.innerText = taskText.value;
+  taskText.value = '';
 }
 
 // eventListeners
