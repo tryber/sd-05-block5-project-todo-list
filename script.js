ebfbb1a1be9398764c004f195436f92206947230
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-24 20:06:29 -0300

    adicionado cursor = pointer nos botões da página

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ let textTask = document.getElementById('texto-tarefa');
 function addTask() {
   let listItem = document.createElement('li');
   listItem.innerHTML = textTask.value;
-  ordenedList.appendChild(listItem);
-  textTask.value = '';
+  if(textTask.value == '' ) {
+    alert('Escreva algo para adicionar');
+  } else{
+    ordenedList.appendChild(listItem);
+    textTask.value = '';
+  }
+}
+
+
+function mouseChangeButton(){
+  let buttons =  document.getElementsByClassName('button')[0];
+  buttons.style.cursor = 'pointer';
 }
 
 buttonTask.addEventListener('click', addTask);
-
+window.addEventListener('load', mouseChangeButton);
