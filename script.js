dc4bee32af58f38b1615f48c452523e0aeb8ad4a
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-24 14:43:15 -0300

    Start JS

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let addButton;
+let taskList;
+let taskInput;
 window.onload = function () {
-
+  addButton = this.document.querySelector('#criar-tarefa');
+  // Define input list
+  taskInput = this.document.querySelector('#texto-tarefa');
+  // Define task list
+  taskList = this.document.querySelector('#lista-tarefas');
+  // Functions
+  addButton.addEventListener('click', function () {
+    let text = taskInput.value;
+    if(text === "") return;
+    let li = document.createElement("li");
+    let newItem = taskList.appendChild(li);
+    newItem.innerHTML = text;
+  });
 };
