06f63ba927a44a7181dc74a42aa0cb9825e3f53c
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-25 18:18:05 -0300

    Adicionando a funcionalidade de seleção de item.

--- script.js
+++ script.js
@@ -2,13 +2,29 @@ const orderedList = document.getElementById('lista-tarefas');
 const buttonCreateTask = document.getElementById('criar-tarefa');
 const taskBox = document.getElementById('texto-tarefa');
 
-function createTask() {
+function createTask(listContainer) {
   const newTask = document.createElement('li');
+  newTask.id = `tarefa-${listContainer.children.length}`;
   newTask.classList.add('li');
+  newTask.classList.add('tarefa');
   newTask.innerText = taskBox.value;
   return newTask;
 }
 
+function removeSelectedClass() {
+  const item = document.getElementsByClassName('selected');
+  if ( item.length > 0) {
+    item[0].classList.remove('selected');
+  }
+}
+
 buttonCreateTask.onclick = function () {
-  orderedList.appendChild(createTask());
+  orderedList.appendChild(createTask(orderedList));
 };
+
+document.addEventListener("click", function (event) {
+  removeSelectedClass();
+  if ( event.target.classList.contains( 'tarefa' ) ) {
+    event.target.classList.add( 'selected' );
+  }
+  })
