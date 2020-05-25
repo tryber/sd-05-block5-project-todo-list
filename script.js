a247dcd5964ebd3d14f4a9f52974e0023352cfc8
Sidnei Ramos <sidneiramos02@gmail.com>
2020-05-25 16:39:40 -0300

    Verify background-color gray

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let list = document.getElementById("lista-tarefas")
 let create = document.getElementById("criar-tarefa")
 let text = document.getElementById("texto-tarefa")
+let selecionado
 
 create.addEventListener("click", function() {
  let task = document.createElement("li")
  task.innerText = text.value
  list.appendChild(task)
  text.value = null
+})
+
+list.addEventListener("click", function () {
+    
+    if (selecionado.classList !== undefined) {
+        selecionado.classList.remove("selected")
+        }
+     event.target.classList.add("selected")
 })
