7a41034f6153325ce968541b5a61e746c2bf7f72
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-24 23:30:10 -0300

    Adicionado função que altera a cor da tarefa selecionada e modificado função clearAll"

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let ordenedList = document.getElementById('lista-tarefas');
 let buttonTask = document.getElementById('criar-tarefa');
 let buttonClear = document.getElementById('apaga-tudo')
 let textTask = document.getElementById('texto-tarefa');
+let colorButton = document.getElementById('colorirb')
 
 function addTask() {
   let listItem = document.createElement('li');
+  listItem.setAttribute('class', 'completed');
   listItem.innerHTML = textTask.value;
   if(textTask.value == '' ) {
     alert('Escreva algo para adicionar');
@@ -12,17 +14,29 @@ function addTask() {
     ordenedList.appendChild(listItem);
     textTask.value = '';
   }
+  addEvents(listItem);
 }
 
-
-
 function clearAllTask() {
-  let allItems = document.querySelector('#lista-tarefas');
-  let listLength = allItems.children.length;
-  
-  for( let i = 0; i < listLength; i += 1) {
-    allItems.removeChild(allItems.children[0]);
-  }
+  ordenedList.innerHTML = '';
+}
+
+function addEvents(listItem) {
+  listItem.addEventListener('click', function(){
+    listItem.style.backgroundColor = corCinza();
+  });
+  listItem.addEventListener('dblclick', function (){
+    if(listItem.style.textDecoration == 'line-through') {
+      listItem.style.textDecoration = 'none';
+    } else{
+      listItem.style.textDecoration = 'line-through';
+    }
+  });
+
+}
+
+function corCinza() {
+  return "rgb(128, 128, 128)";
 }
 
 buttonClear.addEventListener('click', clearAllTask);
