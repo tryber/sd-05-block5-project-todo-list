4a0db20fecfb9b3229d21eba3ae61177aedb478b
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-25 12:59:44 -0300

    Definicao de variaveis e primeiras funçoes no JS

---
+++ script.js
@@ -0,0 +1,40 @@
+//Definição das variáveis básicas
+let addItem = document.getElementById('criar-tarefa');
+let olList = document.getElementById('lista-tarefas');
+let textList = document.getElementById('texto-tarefa');
+let clearAll = document.getElementById('apaga-tudo');
+//Variáveis demais funcionalidades
+let clearFinish = document.getElementById('remover-finalizados');
+let clearSelect = document.getElementById('remover-selecionado');
+let saveList = document.getElementById('salvar-tarefas');
+let moveDown = document.getElementById('mover-baixo');
+let moveUp = document.getElementById('mover-cima');
+let lines = document.getElementById('linhas');
+
+//Funções
+function adicionaItem () {
+  let li = document.createElement("li");
+  olList.appendChild(li);
+  li.innerText = textList.value;
+  textList.value = ""
+
+}
+
+function apagaTarefa() {
+  target = event.target;
+  if (target.classList.contains('completed')) {
+    target.classList.remove('completed');
+  } else {
+    target.classList.add('completed');
+  }
+}
+
+//Apaga todos os itens da lista
+function clear() {
+  olList.innerText = "";
+}
+
+//Event Listners
+addItem.addEventListener("click", adicionaItem);
+clearAll.addEventListener('click', clear);
+olList.addEventListener('dblclick', apagaTarefa);
