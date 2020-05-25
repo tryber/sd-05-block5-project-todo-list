2c2e852a8816c7bbc98d124d8ed1b84fa1b2a4e8
Sidnei Ramos <sidneiramos02@gmail.com>
2020-05-25 16:59:26 -0300

    Repair selected

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let list = document.getElementById("lista-tarefas")
 let create = document.getElementById("criar-tarefa")
 let text = document.getElementById("texto-tarefa")
-let selecionado
+let selecionado = ""
 
 create.addEventListener("click", function() {
  let task = document.createElement("li")
@@ -15,5 +15,6 @@ list.addEventListener("click", function () {
     if (selecionado.classList !== undefined) {
         selecionado.classList.remove("selected")
         }
-     event.target.classList.add("selected")
+    event.target.classList.add("selected")
+    selecionado = event.target
 })
