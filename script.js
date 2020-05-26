11697c5bd5a73d2d86adae36d3c1a64202c1d8c9
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-25 22:34:53 -0300

    Correçao Js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let saveList = document.getElementById('salvar-tarefas');
 let moveDown = document.getElementById('mover-baixo');
 let moveUp = document.getElementById('mover-cima');
 let lines = document.getElementById('linhas');
+let select = "";
 
 //Funções
 function adicionaItem () {
@@ -18,43 +19,11 @@ function adicionaItem () {
   li.innerText = textList.value;
   textList.value = ""
 
-}
-
-function apagaTarefa () {
-  target = event.target;
-  if (target.classList.contains('completed')) {
-    target.classList.remove('completed');
-  } else {
-    target.classList.add('completed');
-  }
-}
-
-//Função para riscar item já concluído
-function itemFinish() {
-  target = event.target; // seta o target
-  if (target.classList.contains('completed')) {
-    // se o target já tem a classe completed
-    target.classList.remove('completed'); // remove a classe completed
-  } else {
-    // se o target não tem a classe completed
-    target.classList.add('completed'); // adiciona a classe completed
-  }
-}
-
 //Apaga todos os itens da lista
 function clear () {
   olList.innerText = "";
 }
 
-//Função para salvar a lista de tarefas
-function save () {
-  localStorage.clear();
-  localStorage.setItem('saved tasks', olList.innerHTML);
-}
-
 //Event Listners
 addItem.addEventListener("click", adicionaItem);
 clearAll.addEventListener('click', clear);
-olList.addEventListener('dblclick', apagaTarefa);
-saveList.addEventListener('click', save); // listener para salvar tasks no localstorage
-olList.addEventListener('dblclick', itemFinish); 
