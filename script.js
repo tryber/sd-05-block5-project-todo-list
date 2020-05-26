6d9028f6e6013f87c623f4f072594de3fdf3ece7
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-26 11:34:33 -0300

    o texto do input e utilizado na funçao e inserido na lista de tarefas

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const botaoCriarTarefa = document.getElementById('criar-tarefa');
 function criarTarefa() {
   const listaTarefas = document.getElementById('lista-tarefas');
   const elementoLista = document.createElement('li');
+  let tarefa = document.getElementById('texto-tarefa').value;
   listaTarefas.appendChild(elementoLista);
+  listaTarefas.lastChild.innerHTML = tarefa;
 }
 botaoCriarTarefa.addEventListener('click', criarTarefa);
