6fae5eeb40c45a8456a8fe96af27989e8ae30366
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-25 21:18:58 -0300

    add botao que remove itens finalizados

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ clickItem.addEventListener('dblclick', function () {
   const itemEscolhido = event.target;
   itemEscolhido.classList.toggle('completed');
 });
+
+const btnApagaTudo = document.querySelector('#apaga-tudo');
+btnApagaTudo.addEventListener('click', function () {
+  const listaTarefas = document.querySelectorAll('li').lenght;
+  for (let i = 0; i < listaTarefas; i += 1) {
+    document.querySelector('ol').removeChild(li);
+  }
+});
+
+const btnFinalizados = document.querySelector('#remover-finalizados');
+btnFinalizados.addEventListener('click', function () {
+  const listaSelecionados = document.querySelectorAll('li');
+  for (let i = 0; i < listaSelecionados.length; i += 1) {
+    if (listaSelecionados[i].classList.contains('completed')) {
+      document.querySelector('#lista-tarefas').removeChild(listaSelecionados[i]);
+    }
+  }
+});
