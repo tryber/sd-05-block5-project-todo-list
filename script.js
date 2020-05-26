94e18b9f84caa86f3bd08881bf1957967ab644f2
Marylange <marylangess@gmail.com>
2020-05-26 15:02:11 -0300

    verificando requisitos

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+const sublinharElemento = (tagLi) => {
+  if (tagLi.style.textDecoration !== 'line-through') {
+    tagLi.style.textDecoration = 'line-through';
+    tagLi.className = 'completed';
+  } else {
+    tagLi.style.textDecoration = 'none';
+    tagLi.className = null;
+  }
+}
+
+const adicionarEventoFundoCinza = (tagLi) => {
+  tagLi.addEventListener('click', () => {
+    tagLi.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+};
+
+const adicionarEventoDeSublinhar = (tagLi) => {
+  tagLi.addEventListener('dblclick', () => {
+    sublinharElemento(tagLi);
+  });
+};
+
 // criar elementos, adicionar na lista e apagar input adicionado
-const criarElementos = () => {
+const criarElementoLista = () => {
   const campoTarefas = document.getElementById('texto-tarefa').value;
   const tagOl = document.querySelector('#lista-tarefas');
   const tagLi = document.createElement('li');
@@ -9,50 +31,37 @@ const criarElementos = () => {
 
   document.getElementById('texto-tarefa').value = '';
 
-  sublinharItem(tagLi);
+  adicionarEventoDeSublinhar(tagLi);
+  adicionarEventoFundoCinza(tagLi);
 };
 
-const btn = document.getElementById('criar-tarefa');
-btn.addEventListener('click', criarElementos);
-
-// Marcar cor dos itens de cinza
-const aplicarCorCinzaAosItens = (tagLi) => {
-  tagLi.addEventListener('click', () => {
-    tagLi.style.backgroundColor = 'rgb(128, 128, 128)';
-  });
-};
-
-// Marcar o item com sublinhado e atribuir a classe completed
-const sublinharItem = (tagLi) => {
-  tagLi.addEventListener('dblclick', () => {
-    for (let i = 0; i < document.getElementsByTagName('li').length; i += 1) {
-      if (tagLi.style.textDecoration !== 'line-through') {
-        tagLi.style.textDecoration = 'line-through';
-        tagLi.className = 'completed';
-      } else {
-        tagLi.style.textDecoration = 'none';
-        tagLi.className = null;
-      }
-    }
-  });
-  aplicarCorCinzaAosItens(tagLi);
-};
-
-// Apagar todos os itens da lista
 const apagarTodosOsItens = () => {
   const tagPai = document.getElementById('lista-tarefas');
   while (tagPai.hasChildNodes()) {
     tagPai.removeChild(tagPai.firstChild);
   };
 };
-const btnApagarTudo = document.getElementById('apaga-tudo');
-btnApagarTudo.addEventListener('click', apagarTodosOsItens);
 
-// Remover itens finalizados
 const removerItensFinalizados = () => {
   while (document.querySelector('.completed')) {
     document.querySelector('.completed').remove();
   };
 };
+
+const btn = document.getElementById('criar-tarefa');
+btn.addEventListener('click', criarElementoLista);
+
+const btnApagarTudo = document.getElementById('apaga-tudo');
+btnApagarTudo.addEventListener('click', apagarTodosOsItens);
+
 let btnItensFinalizados = document.getElementById('remover-finalizados');
 btnItensFinalizados.addEventListener('click', removerItensFinalizados);
+
+// Marcar cor dos itens de cinza
+
+
+// Marcar o item com sublinhado e atribuir a classe completed
+
+
+// Apagar todos os itens da lista
+// Remover itens finalizados
