4c1c9c4c3db4800f5d6b3b16486a8dcfc006afda
luca <luucadecastro@gmail.com>
2020-05-25 22:13:42 -0300

    tentativa de alcançar os 80%

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-let criarTarefa = document.getElementById('criar-tarefa');
-let listaTarefa = document.getElementById('lista-tarefas');
-let inputText = document.getElementById('texto-tarefa');
+const listaTarefa = document.getElementById('lista-tarefas');
+const inputText = document.getElementById('texto-tarefa');
+const button = document.getElementsByClassName('btn');
+var elementList = null;
 
-function criarItem() {
-  let li = document.createElement('li');
+function createItem() {
+  const li = document.createElement('li');
+  li.className='white';
   listaTarefa.appendChild(li);
   li.innerText = inputText.value;
-  inputText.value = "";
+  renderEvents();
 }
 
+function renderEvents() {
+  elementList = listaTarefa.children;
+  for (let index = 0; index < elementList.length; index += 1) {
+    elementList[index].addEventListener('click', changeBgColor)
+  }
+}
+
+function changeBgColor() {
+  console.log(event.target.className == 'white');
+  if (event.target.className == 'grey') {
+    event.target.className = 'white'
+  } else { event.target.className = 'grey'}
+}
+
+function removeAllElements() {
+  while(listaTarefa.firstChild) {
+    listaTarefa.removeChild(listaTarefa.firstChild)
+  }
+}
+// cria uma classe no CSS com a cor
+// e cria um eventListener no ol, pra 
+// adicionar no target a classe selected
