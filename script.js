05acf32601493244f11c97ca79bceabccfcfbbab
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-26 16:40:54 -0300

    event add element added

---
+++ script.js
@@ -0,0 +1,16 @@
+//coded by Lyraah
+//variáveis e constantes
+const button = document.querySelector("#criar-tarefa");
+const caixaDeTexto = document.querySelector("#texto-tarefa");
+let lista = document.querySelector("ol");
+let listaLi = document.querySelectorAll("li");
+
+//funções e métodos
+function add(){
+    const li = document.createElement("li");
+    li.innerText = caixaDeTexto.value;
+    lista.appendChild(li);
+    caixaDeTexto.value = " ";
+}
+//eventos
+button.addEventListener("click",add);
