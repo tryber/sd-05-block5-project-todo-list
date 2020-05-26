4b9da89ae6b85b2c678ce1ee2fcb3030a8117e7f
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-26 16:12:42 -0300

    melhorando a function para transferir input texto neste novo elemento il de ol

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // ao clicar no botão criar-tarefa, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo.
 
 let buttonNewTask = document.getElementById("criar-tarefa");
+let inputText = document.getElementById("texto-tarefa");
 
- function newTask () {
+function newTask() {
   let newTaskList = document.createElement("li");
-  document.getElementById("lista-tarefas").appendChild(newTaskList);
- }
+  document.getElementById("lista-tarefas").appendChild(newTaskList); // para adicionar elemento li no pai ol
+  newTaskList.innerHTML = "" + inputText.value; // para transferir o input texto em novo item de lista
+}
 
- buttonNewTask.addEventListener("click", newTask);
+buttonNewTask.addEventListener("click", newTask);
