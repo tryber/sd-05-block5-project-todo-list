3a44ac12d7888631793f291a73530cae411bb229
fabiano Alves <fasnaweb2020@gmail.com>
2020-05-26 17:32:57 -0300

    requisito 6 concluido

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 
+function criarElemento(){
+    let recuperaOL = document.getElementById('lista-tarefas')
+    let recuperaTexto = document.getElementById('texto-tarefa')
+    let recebeTexto = recuperaTexto.value
+    let criar = document.createElement('li')
+        criar.innerHTML = recebeTexto  
+        document.getElementById('lista-tarefas').appendChild(criar)
+        recuperaTexto.value = " "
+    }
 
+let itemLista = document.getElementById('lista-tarefas');
+
+function selecionarItem(event){
+    let  itemSelecionado = event.target;
+    itemSelecionado.classList.toggle('selected');
+}
+
+itemLista.addEventListener('click',selecionarItem);
 
