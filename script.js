10d37532ddb4b4b7639760995669f92ee638ee51
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-05-26 15:41:40 -0300

    by clicking one time, turn bg grey, by double clicking it, line through it

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ let lista = document.getElementById('lista-tarefas');
 
 buttonAdicionar.addEventListener('click', function () {
   let li = document.createElement('li');
+  li.onclick = function () {
+    li.style.backgroundColor = 'rgb(128,128,128)';
+  };
+  li.ondblclick = function () {
+    let liCompleted = li.getAttribute('class');
+    if (!liCompleted) {
+      li.setAttribute('class', 'completed');
+    } else {
+      li.removeAttribute('class', 'completed');
+    }
+  };
   li.innerHTML = inputText.value;
   lista.appendChild(li);
   inputText.value = '';
-
 });
 
 // (((())))
+// document.getElementsByTagName("H1")[0].removeAttribute("class");
