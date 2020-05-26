94e4ece85d2104d56dac65c80fa82b772d368960
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-26 14:50:13 -0300

    adiçao de linhas explicativas no script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const botaoCriarTarefa = document.getElementById('criar-tarefa');
+// Essa função cria um item na lista de tarefas e insere o texto do input
 function criarTarefa() {
   const listaTarefas = document.getElementById('lista-tarefas');
   const elementoLista = document.createElement('li');
@@ -7,6 +8,7 @@ function criarTarefa() {
   listaTarefas.appendChild(elementoLista);
   listaTarefas.lastChild.innerHTML = tarefa;
 }
+// Função para verificar se o input está vazio antes de adicionar um item
 function verificaInput() {
   if (document.getElementById('texto-tarefa').value === '') {
     alert('Insira alguma tarefa para adicionar à sua Lista!!!');
@@ -17,12 +19,14 @@ function verificaInput() {
 botaoCriarTarefa.addEventListener('click', verificaInput);
 
 const itemLista = document.getElementById('lista-tarefas');
+// adiciona a classe 'selected' ao item clicado
 function selecionaItem(e) {
   const itemSelecionado = e.target;
   itemSelecionado.classList.toggle('selected');
 }
 itemLista.addEventListener('click', selecionaItem);
 
+// risca o item clicado duas vezes
 function riscaItem(e) {
   const itemSelecionado = e.target;
   itemSelecionado.classList.toggle('completed');
@@ -30,12 +34,14 @@ function riscaItem(e) {
 itemLista.addEventListener('dblclick', riscaItem);
 
 const botaoApagaTudo = document.getElementById('apaga-tudo');
+// função para limpar todos os itens
 function apagaTudo() {
   document.getElementById('lista-tarefas').innerHTML = '';
 }
 botaoApagaTudo.addEventListener('click', apagaTudo);
 
 const botaoRemoveFinalizados = document.getElementById('remover-finalizados');
+// função para remover os itens com a classe 'completed'
 function removerFinalizados() {
   const listaTarefas = document.querySelectorAll('#lista-tarefas li');
   for (let i = 0; i < listaTarefas.length; i += 1) {
@@ -47,6 +53,7 @@ function removerFinalizados() {
 botaoRemoveFinalizados.addEventListener('click', removerFinalizados);
 
 const botaoRemoveSelecionado = document.getElementById('remover-selecionado');
+// função que remove os itens com a classe 'selected'
 function removerSelecionado() {
   const listaTarefas = document.querySelectorAll('#lista-tarefas li');
   for (let i = 0; i < listaTarefas.length; i += 1) {
