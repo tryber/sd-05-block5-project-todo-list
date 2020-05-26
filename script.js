02f3119baeff17e3891c8b6f9386151fae6bac90
Christian Duessel <duessel@gmail.com>
2020-05-26 13:24:58 -0300

    selecionar e completar

--- script.js
+++ script.js
@@ -4,15 +4,36 @@
 // limpar input
 console.log("link on?")
 
- function criarTarefas () {
+function criarTarefas () {
 	let filha = document.createElement("li");
 	const tarefa = document.querySelector("#texto-tarefa").value;
 	filha.innerHTML = tarefa;
  	document.getElementById("lista-tarefas").appendChild(filha);
-	 document.querySelector("#texto-tarefa").value="";
+	document.querySelector("#texto-tarefa").value="";
 };
 
 document.getElementById("criar-tarefa").addEventListener("click", criarTarefas);
 
+// clicar elemento da lista alterar fundo para cinza rgb(128,128,128) e voltar
 
+function selecaoitem (event){
+	let item = event.target;
+	if(item.classList.contains('selected')){
+		item.classList.remove('selected');
+	}
+	else{item.classList.add('selected')};
+}
 
+document.getElementById("lista-tarefas").addEventListener("click", selecaoitem);
+
+// clicar duas vezes elemento da lista riscar ele e voltar
+
+function completedItem (event){
+	let Citem = event.target;
+	if(Citem.classList.contains('completed')){
+		Citem.classList.remove('completed');
+	}
+	else{Citem.classList.add('completed')};
+}
+
+document.getElementById("lista-tarefas").addEventListener("dblclick", completedItem);
