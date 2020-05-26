6236283f53d3daa060cba8e4b76a75026ed53f46
fabiano Alves <fasnaweb2020@gmail.com>
2020-05-26 18:34:23 -0300

    requisito nove completado

--- script.js
+++ script.js
@@ -10,12 +10,22 @@ function criarElemento(){
     }
 
 
+
 let itemLista = document.getElementById('lista-tarefas');
 
 function selecionarItem(event){
     let  itemSelecionado = event.target;
     itemSelecionado.classList.toggle('selected');
 }
-
 itemLista.addEventListener('click',selecionarItem);
+
+
+let riscaItemDaLista = document.getElementById('lista-tarefas');
+
+function selecionarItParaRiscar(event){
+    let itemSelecionado2 = event.target
+    itemSelecionado2.classList.toggle('completed');
+}
+
+riscaItemDaLista.addEventListener('dblclick',selecionarItParaRiscar);
 
