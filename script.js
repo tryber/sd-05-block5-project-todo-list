ed50b94f6f786b7350208ea27f15b2d1bec7bfe0
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-26 17:05:36 -0300

    style cursor added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const caixaDeTexto = document.querySelector("#texto-tarefa");
 let lista = document.querySelector("ol");
 let listaLi = document.querySelectorAll("li");
 
+
 //funções e métodos
 function add(){
     const li = document.createElement("li");
@@ -12,5 +13,10 @@ function add(){
     lista.appendChild(li);
     caixaDeTexto.value = "";
 }
+function mouse(){
+    (lista.target).style.cursor = 'pointer';
+  };
+
 //eventos
 button.addEventListener("click",add);
+lista.addEventListener('mouseover', mouse);
