be8ed4eefd288b6ee90e246084fae87914267e85
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-26 16:48:06 -0300

    function clear adjusted

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function add(){
     const li = document.createElement("li");
     li.innerText = caixaDeTexto.value;
     lista.appendChild(li);
-    caixaDeTexto.value = " ";
+    caixaDeTexto.value = "";
 }
 //eventos
 button.addEventListener("click",add);
