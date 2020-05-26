b221daaedc22fafc4ee63ae600af97e3fe39bbaa
Mau <mauricio.sdjr@gmail.com>
2020-05-26 11:31:15 -0300

    insert add list item button functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+let addButton = document.getElementById("criar-tarefa");
+let textInput = document.getElementById("texto-tarefa");
+let listOfItems = document.getElementById("lista-tarefas");
+
+
+addButton.addEventListener("click", addItemList);
+
+function addItemList() {
+  let listItems = document.createElement("li");
+  listOfItems.appendChild(listItems);
+  listItems.innerText = textInput.value;
+  textInput.value = null;
+}
