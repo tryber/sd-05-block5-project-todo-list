efb8d58e2ad4c36f40d5fff7b1153fcda569b529
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-25 23:00:48 -0300

    Bonus done

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function moveUp() {
 }
 function moveDown() {
   const itens = taskList.children;
-  for (let i = itens.length-2; i >= 0; i -= 1) {
+  for (let i = itens.length - 2; i >= 0; i -= 1) {
     if (itens[i].className.includes('selected')) {
       const currentItem = itens[i + 1].innerHTML;
       taskList.children[i + 1].innerHTML = taskList.children[i].innerHTML;
@@ -49,11 +49,11 @@ function saveList() {
   const itens = taskList.children;
   const list = [];
   for (let i = 0; i < itens.length; i += 1) list[i] = [itens[i].innerHTML, itens[i].className];
-  if (typeof(Storage) !== 'undefined') localStorage.setItem('list', JSON.stringify(list));
-  else console.error('Este browser não tem suporte a Storage');
+  if (typeof (Storage) !== 'undefined') localStorage.setItem('list', JSON.stringify(list));
+  else window.alert('Este browser não tem suporte a Storage');
 }
 function loadList() {
-  if (typeof(Storage) !== 'undefined') {
+  if (typeof (Storage) !== 'undefined') {
     const itens = JSON.parse(localStorage.getItem('list'));
     if (!itens) {
       window.alert('Não há nenhuma lista salva');
