c551a056043e200e21e3f5f32915ccec05d8c6f3
Mau <mauricio.sdjr@gmail.com>
2020-05-26 15:32:22 -0300

    add bg color grey functionality

--- script.js
+++ script.js
@@ -9,5 +9,14 @@ function addItemList() {
   let listItems = document.createElement("li");
   listOfItems.appendChild(listItems);
   listItems.innerText = textInput.value;
-  textInput.value = null;
+  textInput.value = "";
 }
+
+//Change list item bg color to grey when clicked
+function changeBGColor() {
+  let tagLI = document.getElementsByTagName("li");
+  for(let i = 0; i < tagLI.length; i += 1) {
+  event.target.style.backgroundColor = "rgb(128, 128, 128)";
+  } 
+}
+listOfItems.addEventListener("click", changeBGColor);
