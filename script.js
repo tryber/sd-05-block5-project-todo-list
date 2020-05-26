09caab45e4418ddc830079b3f777dd313c50caad
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-26 12:03:11 -0300

    Estilizaçao dos itens da lista e alteraçao do cursor na lista e botoes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function criarTarefa() {
   const listaTarefas = document.getElementById('lista-tarefas');
   const elementoLista = document.createElement('li');
   let tarefa = document.getElementById('texto-tarefa').value;
+  document.getElementById('texto-tarefa').value = '';
   listaTarefas.appendChild(elementoLista);
   listaTarefas.lastChild.innerHTML = tarefa;
 }
@@ -14,3 +15,16 @@ function verificaInput() {
   }
 }
 botaoCriarTarefa.addEventListener('click', verificaInput);
+
+const itemLista = document.getElementById('lista-tarefas');
+function selecionaItem(e) {
+  let itemSelecionado = e.target;
+  itemSelecionado.classList.toggle('selected');
+}
+itemLista.addEventListener('click', selecionaItem);
+
+function riscaItem(e) {
+  let itemSelecionado = e.target;
+  itemSelecionado.classList.toggle('completed');
+}
+itemLista.addEventListener('dblclick', riscaItem);
