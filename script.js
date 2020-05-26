2cd8ce5cf146ec0e1191240ddc652658b4e13044
Rafael Assad <rafaassadeng@gmail.com>
2020-05-26 16:21:57 -0300

    alguns botoes

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let addButton = document.querySelector("#criar-tarefa");
 let inputText = document.querySelector("#texto-tarefa");
 let list = document.querySelector("#lista-tarefas");
+let eraseButton = document.querySelector("#apaga-tudo");
 
 function createList(){
     let li = document.createElement('li');
     li.innerHTML = inputText.value;
     list.appendChild(li);
+    inputText.value = '';
 }
 
 addButton.addEventListener('click', createList);
+
+function eraseList(){
+    list.innerHTML = '';
+}
+
+eraseButton.addEventListener('click', eraseList);
