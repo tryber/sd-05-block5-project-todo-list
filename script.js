226cbfdd62176c56d3270b72515a942070aba926
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-05-27 15:39:12 -0300

    terminado o esqueleto do html para a introdução do css e js.

---
+++ script.js
@@ -0,0 +1,21 @@
+// Deve haver uma lista ordenada de tarefas com o id="lista-tarefas"
+// Os itens da lista de tarefas devem ser ordenados por ordem de criação.
+// Ao passar o mouse sobre qualquer botão e qualquer item da lista
+
+// o ícone do mouse deve mudar para uma mãozinha apontando-o.
+
+// Ao clicar em um item da lista, altere a cor de fundo do item para cinza rgb(128,128,128).
+
+// 9 - Ao clicar duas vezes em um item, ele deverá ser riscado,
+// indicando que foi completo. Deve ser possível desfazer essa ação
+// clicando novamente duas vezes no item.
+// Pontos importantes sobre este requisito:
+// * Crie uma classe CSS com o nome "completed" e defina a propriedade
+// "text-decoration" com o valor "line-through".
+// * Utilize a classe CSS "completed" para adicionar o efeito de letra
+// tachada (riscada) as tarefas finalizadas.
+
+// BONUS
+
+// 1 - Adicione um botão com id="salvar-tarefas" que salve o conteúdo da lista.
+// Se você fechar e reabrir a página, a lista deve continuar no estado em que estava.
