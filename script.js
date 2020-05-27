50b5d7e97df0b86bb3e897acfe4ad80667e202f6
Larissa Palombo <larissampalombo@gmail.com>
2020-05-27 20:24:43 -0300

    botao inserir

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const botaoAdd = document.getElementById ('criar-tarefa');
 const botaoClearAll = document.getElementById ('apaga-tudo');
 const botaoClear = document.getElementById ('remover-finalizados');
 const tarefa = document.getElementById ('texto-tarefa');
-const lista = document.getElementsByTagName ('li');
+const lista = document.getElementById ('lista-tarefas');
 let a = "";
 
 botaoAdd.addEventListener ('click', adicionar);
 
 function adicionar () {
-    if (tarefa != " ") {
-        lista.innerHTML = tarefa.value;   
+    if (tarefa.value !== " ") {
+        const li = document.createElement ('li');
+        li.innerText = tarefa.value;
+        lista.appendChild (li);  
         tarefa.value = " ";
     }
 
