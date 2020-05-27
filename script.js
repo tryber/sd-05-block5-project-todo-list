bd55009353332fc1d8b7da4d467578116472991d
Cass007 <thecastieel07@gmail.com>
2020-05-27 20:12:40 -0300

    criada função de adicionar1

--- script.js
+++ script.js
@@ -2,19 +2,16 @@ let lista = document.getElementById("lista-tarefas");
 let textoEntrada = document.getElementById("texto-tarefa");
 let btn = document.getElementById("criar-tarefa");
 
-btn.addEventListener("click",addList)
+btn.addEventListener("click", addList);
 
-
-function addList(){
+function addList() {
   let add = document.createElement("li");
   add.innerText = textoEntrada.value;
   lista.appendChild(add);
-  textoEntrada.value ="";
+  textoEntrada.value = "";
   textoEntrada.focus();
-};
+}
 
-textoEntrada.addEventListener("keyup",function(e){
- if(e.keyCode === 13)
- addList()
-})
-
+textoEntrada.addEventListener("keyup", function (e) {
+  if (e.keyCode === 13) addList();
+});
