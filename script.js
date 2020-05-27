94c22b23f5243075d7aaa97d58c233747348781e
pauloricardoz <trybe.przt@gmail.com>
2020-05-27 19:19:24 -0300

    mouse pointer

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-botao=document.getElementById("criar-tarefa")
-lista=document.getElementById("lista-tarefas")
-entradaTexto=document.getElementById("texto-tarefa")
-botao.addEventListener('click',function(){
-  let novo=document.createElement("li")
-  novo.innerText=entradaTexto.value;
+botao = document.getElementById("criar-tarefa")
+lista = document.getElementById("lista-tarefas")
+entradaTexto = document.getElementById("texto-tarefa")
+botao.addEventListener('click', function () {
+  let novo = document.createElement("li")
+  novo.innerText = entradaTexto.value;
   lista.appendChild(novo)
-  entradaTexto.value=""
+  entradaTexto.value = ""
+})
 
+lista.addEventListener('mouseover', function () {
+  document.body.style.cursor = 'pointer'
 })
