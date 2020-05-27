5cafb3ca90aa712678cb083fc2dbcf64ef41fe3a
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-05-27 19:08:32 -0300

    inclusão dos primeiros estilos

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// variáveis 
+//variáveis
 const textoInput = document.getElementById('texto-tarefa');
 const listaOrdenada = document.getElementById('lista-tarefas');
 const criaTarefa = document.getElementById('criar-tarefa');
-const apagaItens = document.getElementById('apaga-tudo')
+const apagaItens = document.getElementById('apaga-tudo');
 
-function adicionaItem () {
+function adicionaItem() {
   const criaItem = document.createElement('li');
-    criaItem.innerHTML = textoInput.value;
+  criaItem.innerHTML = textoInput.value;
   if (textoInput.value === '') {
-    alert('Digite um item para adicionar à lista')
+    alert('Digite um item para adicionar à lista');
   } else {
     listaOrdenada.appendChild(criaItem);
     textoInput.value = '';
