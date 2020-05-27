609aef249164c9d3c7c4046f76ae15c8440df313
preto115 <preto115@gmail.com>
2020-05-27 17:22:18 -0300

    funcao pra adicionar implementada

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Feito por Elisangelo
+let listaTarefas = document.getElementById("lista-tarefas");
+let input = document.getElementById("texto-tarefa");
+let adicionar = document.getElementById("criar-tarefa");
+        
+adicionar.addEventListener("click", adicionaLista)
+        
+function adicionaLista(){
+             
+        let lista = document.createElement("li");
+        lista.innerHTML = input.value;
+        document.body.appendChild(lista);
+        input.value = "";
+        }
+        
