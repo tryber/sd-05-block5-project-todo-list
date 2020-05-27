d7f396095be51676d7e56b63beaa47703865090b
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-05-26 23:24:53 -0300

    Até o item 9

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const posBotaoAdicionar = document.getElementById('criar-tarefa');
 posBotaoAdicionar.addEventListener("click", adicionaItem);
 let conteudoAdicionar = '';
 const posTextoTarefa = document.getElementById('texto-tarefa');
-
 let posListaTarefas = document.getElementById('lista-tarefas');
 
 
-document.addEventListener('mouseover', function (event) {
-  if (event.target.nodeName == 'BUTTON' || event.target.nodeName == 'LI') {
-    event.target.style.cursor = 'pointer';
+posListaTarefas.addEventListener('click', coloreCinza);
+posListaTarefas.addEventListener('dblclick', adicionaCompleted);
+
+function adicionaCompleted(event){
+  event.target.classList.toggle('completed');
+}
+
+function coloreCinza (event) {
+  const pegaTodosLi = document.querySelectorAll('#lista-tarefas li');
+  for (i = 0; i<pegaTodosLi.length; i+=1){
+    if (pegaTodosLi[i].classList.contains('coloreCinza')) {
+      pegaTodosLi[i].classList.remove('coloreCinza');
+    }
   }
-}, false);
+  event.target.classList.add('coloreCinza');
+}
 
 function adicionaItem () {
   const criaLi = document.createElement('li');
