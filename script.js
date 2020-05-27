2df7030039052005c20828d76a5952362c67f444
pauloricardoz <trybe.przt@gmail.com>
2020-05-27 18:00:09 -0300

    adding buttom, and eventListener

---
+++ script.js
@@ -0,0 +1,10 @@
+botao=document.getElementById("criar-tarefa")
+lista=document.getElementById("lista-tarefas")
+entradaTexto=document.getElementById("texto-tarefa")
+botao.addEventListener('click',function(){
+  let novo=document.createElement("li")
+  novo.innerText=entradaTexto.value;
+  lista.appendChild(novo)
+  entradaTexto.value=""
+
+})
