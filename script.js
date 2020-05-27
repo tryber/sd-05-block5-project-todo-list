f4cc47529ab62dace911cdc0c8f63e0286e49ba0
RenataNovais <rnovaiscs@gmail.com>
2020-05-26 21:54:35 -0300

    Attending bonus requirement number 3 - deleting selected taks item

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ const taskItem = document.getElementsByTagName('li');
 // O texto do item da lista criado, deve ser o texto inserido no text-input.//
 
 function addTask() {
-  const li = document.createElement('li');
-  li.innerHTML = textInput.value;
-  taskList.appendChild(li);
-  textInput.value = '';
-  // Ao clicar em um item da lista, altere a cor de fundo do item para cinza rgb(128,128,128).
-  li.addEventListener('click', function () {
-    li.style.backgroundColor = 'rgb(128 , 128 , 128)';
-  });
-  li.addEventListener('dblclick', function () {
-    li.classList.toggle('completed');
-  });
+  if (textInput.value === '') {
+    alert('Preencha a caixa de texto com sua tarefa!');
+  } else {
+    const li = document.createElement('li');
+    li.innerHTML = textInput.value;
+    taskList.appendChild(li);
+    textInput.value = '';
+    // Ao clicar em um item da lista, altere a cor de fundo do item para cinza rgb(128,128,128).
+    li.addEventListener('click', function () {
+      li.classList.toggle('selected');
+    });
+    li.addEventListener('dblclick', function () {
+      li.classList.toggle('completed');
+    });
+  }
 }
 
 function deleteList() {
@@ -34,3 +38,19 @@ function deleteCompleted() {
     completedElements[0].parentNode.removeChild(completedElements[0]);
   }
 }
+
+function removeSelected() {
+  let selectedElements = document.getElementsByClassName('selected');
+  while (selectedElements.length > 0) {
+    selectedElements[0].parentNode.removeChild(selectedElements[0]);
+  }
+}
+
+function saveList() {
+  for (let i = 0; i < taskItem.length; i += 1) {
+    console.log(taskItem[i].innerHTML);
+    window.localStorage.setItem('item', taskItem[i].innerHTML);
+  }
+}
+
+window.localStorage.getItem('item');
