8c7c485b340a66a2e8ebdbe2879cc0e3f3c072bf
Thiago <asm.thiago@gmail.com>
2020-05-27 18:02:43 -0300

    Add text list

---
+++ script.js
@@ -0,0 +1,12 @@
+let textoTarefa = document.getElementById('texto-tarefa');
+let criarTarefas = document.getElementById('criar-tarefas');
+let listaTarefas = document.getElementById('lista-tarefas');
+let apagaTudo = document.getElementById('apaga-tudo');
+let removerFinalizados = document.getElementById('remover-finalizados');
+
+criarTarefas.addEventListener('click', function () {
+  let tarefa = document.createElement("li");
+  listaTarefas.appendChild(tarefa);
+  tarefa.innerText = textoTarefa.value;
+  textoTarefa.value = ""
+});
