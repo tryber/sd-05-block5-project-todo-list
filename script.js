128abd8809e16d5021e799ce0f0299760a7ac2a2
Thiago <asm.thiago@gmail.com>
2020-05-27 20:02:30 -0300

    Background Cinza e Ristar Tarefa

--- script.js
+++ script.js
@@ -6,8 +6,25 @@ let removerFinalizados = document.getElementById('remover-finalizados');
 
 criarTarefas.addEventListener('click', function () {
   let tarefa = document.createElement("li");
-  listaTarefas.appendChild(tarefa);
-  tarefa.innerText = textoTarefa.value;
+  let itemDaLista = listaTarefas.appendChild(tarefa);
+  itemDaLista.innerText = textoTarefa.value;
   textoTarefa.value = ""
+  itemDaLista.addEventListener('click', function(evento){
+    if (evento.target.className.includes('backgroundClick')){
+      evento.target.classList.remove('backgroundClick');
+    } else {
+      let conferirBackground = document.querySelector('.backgroundClick');
+      if (conferirBackground){
+        conferirBackground.classList.remove('backgroundClick')
+      }
+      evento.target.classList.add('backgroundClick')
+    }
+  })
+  itemDaLista.addEventListener('dblclick', function(evento){
+    if (evento.target.className.includes('riscarLetra')){
+      evento.target.classList.remove('riscarLetra');
+    } else {
+      evento.target.classList.add('riscarLetra')
+    }
+  })
 });
-
