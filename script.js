dd643ebf832d01748c3180be655bb6dc07b9fac2
preto115 <preto115@gmail.com>
2020-05-28 12:53:56 -0300

    atualizando a funcao selecionarTarefas para funcionar como deve

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 // Feito por Elisangelo
-//Declaracao de variaveis
+//Declaracao de variaveis globais
 let listaTarefas = document.getElementById("lista-tarefas");
-let input = document.getElementById("texto-tarefa");
-let adicionar = document.getElementById("criar-tarefa");
-let apagaTudo = document.getElementById("apaga-tudo");
-// adicionando eventos nos botoes e campos de texto        
-listaTarefas.addEventListener('click', fundoCinza); 
-listaTarefas.addEventListener('dblclick', listaRiscada);
-apagaTudo.addEventListener('click', excluirLista);
-adicionar.addEventListener("click", adicionaLista);
-input.addEventListener("keypress", function(e){
+let inputTexto = document.getElementById("texto-tarefa");
+let botaoAdicionar = document.getElementById("criar-tarefa");
+let botaoApagaTudo = document.getElementById("apaga-tudo");
+let lista = document.getElementsByTagName("li");
+
+// adicionando eventos ao apertar Enter nos campos de texto      
+inputTexto.addEventListener("keypress", function(e){
      if( e.keyCode === 13 ){
          adicionaLista();
      }
  });
+//adicionando evento ao click do botao (adicionar)  para chamar a funcao adicionaLista()
+botaoAdicionar.addEventListener("click", adicionaLista);
 function adicionaLista(){
-        let adicionarLista = document.createElement("li");
-        adicionarLista.innerHTML = input.value;
-        listaTarefas.appendChild(adicionarLista);
-        input.value = "";
+    var item = document.createElement("li");
+        item.innerHTML = inputTexto.value;
+        listaTarefas.appendChild(item);
+        inputTexto.value = "";
+        inputTexto.focus();
 }
-function fundoCinza(event){
-    let sombra = event.target;
-    sombra.classList.toggle("selected");
+//adiconando evento a tag (ol) para quando houver um Click, chamar a funcao selecionaTarefa()
+listaTarefas.addEventListener('click', selecionaTarefa); 
+function selecionaTarefa(event){
+    let alvo = event.target;
+    for(item = 0; item < lista.length; item +=1){
+        if (!(alvo.classList.contains("selected")) && alvo === lista[item]){
+            lista[item].classList.add("selected");
+        }
+        else {
+                lista[item].classList.remove("selected");
+        }
+    }
 }
-function listaRiscada(event){
+//adiconando evento a tag (ol) para quando houver um duploClick, chamar a funcao riscarTarefa()
+listaTarefas.addEventListener('dblclick', riscarTarefa);
+function riscarTarefa(event){
     let risco = event.target;
     risco.classList.toggle("completed");  
 }
+//adiconando evento ao botao (apagaTudo) para quando houver um Click, chamar a funcao exluirTarefa()
+botaoApagaTudo.addEventListener('click', excluirLista);
 function excluirLista(){
     listaTarefas.innerHTML = "";
 }
