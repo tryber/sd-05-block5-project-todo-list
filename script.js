4ed37ba668868d8e2a4e3e196713c77fd2939e52
Larissa Palombo <larissampalombo@gmail.com>
2020-05-28 11:09:22 -0300

    botao limpar selecionados funcionando

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const botaoClearAll = document.getElementById ('apaga-tudo');
 const botaoClear = document.getElementById ('remover-finalizados');
 const tarefa = document.getElementById ('texto-tarefa');
 const lista = document.getElementById ('lista-tarefas');
-let a = "";
 
 
 botaoAdd.addEventListener ('click', adicionar);
 botaoClearAll.addEventListener ('click', resetar);
+botaoClear.addEventListener ('click', rmvSel);
 
 function adicionar () {
     if (tarefa.value !== "") {
@@ -35,5 +35,11 @@ function resetar () {
     while (lista.lastElementChild) {
         lista.removeChild (lista.lastElementChild);
     }    
+}
+let elementosSel = document.getElementsByClassName ('selected');
+function rmvSel () {
+    for (let i = elementosSel.length -1; i>=0; i-=1) {
+        elementosSel[i].remove();
+    } 
 }
 
