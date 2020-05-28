dfff651fec62d9929ce8d7004bb6bb1abb292922
preto115 <preto115@gmail.com>
2020-05-27 22:10:09 -0300

    implementando script pra aceitar tanto tc Enter + mouse

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ let input = document.getElementById("texto-tarefa");
 let adicionar = document.getElementById("criar-tarefa");
 let apagaTudo = document.getElementById("apaga-tudo");
         
-adicionar.addEventListener("click", adicionaLista);
+
 listaTarefas.addEventListener('click', fundoCinza); 
 listaTarefas.addEventListener('dblclick', listaRiscada);
 apagaTudo.addEventListener('click', excluirLista);
+adicionar.addEventListener("click", adicionaLista);
+
 
 function adicionaLista(){
-             
         let adicionarLista = document.createElement("li");
         adicionarLista.innerHTML = input.value;
         listaTarefas.appendChild(adicionarLista);
         input.value = "";
-        }
+}
 function fundoCinza(){
     let sombra = event.target;
     sombra.classList.toggle("fundoCinza");
@@ -25,6 +26,11 @@ function listaRiscada(){
     risco.classList.toggle("completed");  
 }
 function excluirLista(){
-    let limparLista = event.target;
-    listaTarefas.innerHTML = " ";
+    listaTarefas.innerHTML = " qq";
 }
+input.addEventListener("keypress", function(e){
+   // console.log(e.keyCode)
+    if( e.keyCode === 13 ){
+        adicionaLista();
+    }
+});
