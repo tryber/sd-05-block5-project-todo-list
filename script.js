021027169caec96b92edcc3f2261934fb73bd85c
preto115 <preto115@gmail.com>
2020-05-27 22:48:10 -0300

    ajustando caracter de espaco para vazio

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 // Feito por Elisangelo
+//Declaracao de variaveis
 let listaTarefas = document.getElementById("lista-tarefas");
 let input = document.getElementById("texto-tarefa");
 let adicionar = document.getElementById("criar-tarefa");
 let apagaTudo = document.getElementById("apaga-tudo");
-        
-
+// adicionando eventos nos botoes e campos de texto        
 listaTarefas.addEventListener('click', fundoCinza); 
 listaTarefas.addEventListener('dblclick', listaRiscada);
 apagaTudo.addEventListener('click', excluirLista);
 adicionar.addEventListener("click", adicionaLista);
-
-
+input.addEventListener("keypress", function(e){
+     if( e.keyCode === 13 ){
+         adicionaLista();
+     }
+ });
 function adicionaLista(){
         let adicionarLista = document.createElement("li");
         adicionarLista.innerHTML = input.value;
         listaTarefas.appendChild(adicionarLista);
         input.value = "";
 }
-function fundoCinza(){
+function fundoCinza(event){
     let sombra = event.target;
-    sombra.classList.toggle("fundoCinza");
+    sombra.classList.toggle("selected");
 }
-function listaRiscada(){
+function listaRiscada(event){
     let risco = event.target;
     risco.classList.toggle("completed");  
 }
 function excluirLista(){
-    listaTarefas.innerHTML = " qq";
+    listaTarefas.innerHTML = "";
 }
-input.addEventListener("keypress", function(e){
-   // console.log(e.keyCode)
-    if( e.keyCode === 13 ){
-        adicionaLista();
-    }
-});
