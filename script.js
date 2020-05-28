077c97118a52034e24bbfa83fe8b2b912addbd1c
pauloricardoz <trybe.przt@gmail.com>
2020-05-27 22:31:56 -0300

    codeclime1

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-let botao = document.getElementById('criar-tarefa')
-let lista = document.getElementById('lista-tarefas')
-let entradaTexto = document.getElementById('texto-tarefa')
+const botao = document.getElementById('criar-tarefa');
+const lista = document.getElementById('lista-tarefas');
+const entradaTexto = document.getElementById('texto-tarefa');
 botao.addEventListener('click', function () {
-  let novo = document.createElement('li')
+  let novo = document.createElement('li');
   novo.innerText = entradaTexto.value;
-  lista.appendChild(novo)
-  entradaTexto.value = ''
-})
+  lista.appendChild(novo);
+  entradaTexto.value = '';
+});
 lista.addEventListener('mouseover', function () {
-  document.body.style.cursor = 'pointer'
-})
+  document.body.style.cursor = 'pointer';
+});
 lista.addEventListener('click', function (event) {
-  let selected = event.target
+  let selected = event.target;
   for (let i = 0; i < selected.parentElement.children.length; i++) {
     if (selected.parentElement.children[i].className.indexOf('selected') >= 0) {
-      selected.parentElement.children[i].className = selected.parentElement.children[i].className.replace('selected', '')
+      selected.parentElement.children[i].className = selected.parentElement.children[i].className.replace('selected', '');
     }
   }
   if (selected.className.indexOf('selected') < 0) {
-    selected.className += ' selected'
+    selected.className += ' selected';
   }
-})
+});
 //
 lista.addEventListener('dblclick', function (event) {
-  let selected = event.target
+  let selected = event.target;
   if (selected.className.indexOf(' completed') < 0) {
-    selected.className += ' completed'
+    selected.className += ' completed';
   } else {
-    selected.className = selected.className.replace(' completed', '')
+    selected.className = selected.className.replace(' completed', '');
   }
-})
-let botaoApagaTudo = document.getElementById('apaga-tudo')
+});
+let botaoApagaTudo = document.getElementById('apaga-tudo');
 botaoApagaTudo.addEventListener('click', function () {
-  document.getElementById('lista-tarefas').innerHTML = ''
+  document.getElementById('lista-tarefas').innerHTML = '';
 })
-let botaoFinalizados = document.getElementById('remover-finalizados')
+let botaoFinalizados = document.getElementById('remover-finalizados');
 botaoFinalizados.addEventListener('click', function () {
-  let listFinalizados = document.getElementsByClassName('completed')
+  let listFinalizados = document.getElementsByClassName('completed');
   for (let item = listFinalizados.length - 1; item >= 0; item--) {
-    let elemento = listFinalizados[item]
-    elemento.parentElement.removeChild(elemento)
+    let elemento = listFinalizados[item];
+    elemento.parentElement.removeChild(elemento);
   }
 })
-let botaoRemoveSelecionado = document.getElementById('remover-selecionado')
+let botaoRemoveSelecionado = document.getElementById('remover-selecionado');
 botaoRemoveSelecionado.addEventListener('click', function () {
-  let selecionadoRemocao = document.getElementsByClassName('selected')
+  let selecionadoRemocao = document.getElementsByClassName('selected');
   for (let item = selecionadoRemocao.length - 1; item >= 0; item--) {
-    let elemento = selecionadoRemocao[item]
-    elemento.parentElement.removeChild(elemento)
+    let elemento = selecionadoRemocao[item];
+    elemento.parentElement.removeChild(elemento);
   }
 })
-let salvaLista = document.getElementById('salvar-tarefas')
+let salvaLista = document.getElementById('salvar-tarefas');
 salvaLista.addEventListener('click', function () {
-  localStorage.setItem('lista', document.getElementsByTagName('ol')[0].innerHTML)
-})
+  localStorage.setItem('lista', document.getElementsByTagName('ol')[0].innerHTML);
+});
 if (localStorage.getItem('lista') != null) {
-  document.getElementsByTagName('ol')[0].innerHTML = localStorage.getItem('lista')
+  document.getElementsByTagName('ol')[0].innerHTML = localStorage.getItem('lista');
 }
