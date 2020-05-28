5b79d5269bd91ce76bc3b16400107d8e95c36edd
Elisa <elisatcamillo@gmail.com>
2020-05-28 11:20:58 -0300

    added js variables

---
+++ script.js
@@ -0,0 +1,6 @@
+let criar_tarefa = document.getElementById("criar-tarefa");
+let salvar_tarefas = document.getElementById("salvar-tarefas");
+let remover_finalizados = document.getElementById("remover-finalizados");
+let apaga_tudo = document.getElementById("apaga-tudo");
+let mover_cima = document.getElementById("mover-cima");
+let mover_baixo = document.getElementById("mover-baixo")
