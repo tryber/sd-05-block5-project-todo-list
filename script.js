27b06a291d3bf324a925832131d1ca35e91ad17e
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-28 20:23:46 -0300

    Corrected id funcionamento

---
+++ script.js
@@ -0,0 +1,12 @@
+let addButton = document.getElementsByName("button")[0];
+addButton.addEventListener("click", addItem);
+
+function addItem(){
+  let list = document.getElementsByTagName("ol")[0];
+  let textoTarefa = document.getElementById("texto-tarefa");
+  let text = textoTarefa.value;
+  let li = document.createElement("li");
+  li.innerHTML = text;
+  list.appendChild(li);
+}
+
