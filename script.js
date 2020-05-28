2294624bede72963b1b0c18a684b8ee7f7aa2c95
Lucas Staroscky <staroscky60@gmail.com>
2020-05-28 19:20:49 -0300

    add trigger to create a task and double click to selecte with gray brackground

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 window.onload = function () {
 
- const buttonHTML = document.getElementsByTagName('button')
- const creakeTaskButton = document.getElementsByTagName("criar-tarefa");
+ const buttonHTML = document.getElementsByTagName('button');
+ const listHTML = document.getElementsByTagName('ol');
+ const createTaskButton = document.getElementById("criar-tarefa");
+ const inputValue = document.getElementById('texto-tarefa');
 
- // adding the cursor pointer on the buttons
- const cursosFunction = () => {
-  for (let i = 0; i < buttonHTML.length; i += 1) {
-   let index = buttonHTML[i];
+
+
+
+ // function to add the cursor pointer
+ const cursorFunction = (event) => {
+  for (let i = 0; i < event.length; i += 1) {
+   let index = event[i];
    index.style.cursor = "pointer";
-
   }
  }
+
+ //  button cursor function 
+ cursorFunction(buttonHTML)
+
+ //  list cursor function
+ cursorFunction(listHTML)
+
+
+ // // trigger event to creake a task
+ //  const orderList = document.querySelectorAll('ol')[0];
+ //  const textValue = inputValue.value;
+ //  let list = document.createElement('li');
+ //  list.ondblclick = () => (list.style.backgroundColor = "grey")
+ //  list.innerHTML = textValue;
+ //  orderList.appendChild(list);
+ //  inputValue.value = '';
+
+  
+  const orderList = document.querySelectorAll('.lista-tarefas');
+  const textValue = inputValue.value;
+  let list = document.createElement('li');
+  createTaskButton.addEventListener("click", () => {
+   list.innerHTML = textValue;
+   orderList.appendChild(list);
+   inputValue.value = '';
+  });
+  
+  
+  list.ondblclick = () => (list.style.backgroundColor = "grey")
+
 
 
 }
