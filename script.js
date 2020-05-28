e256a203369c24ea8fd39e3391376defa4382de4
Larissa Palombo <larissampalombo@gmail.com>
2020-05-27 23:09:16 -0300

    mais um pouco do botao apagar finalizadas

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const tarefa = document.getElementById ('texto-tarefa');
 const lista = document.getElementById ('lista-tarefas');
 let a = "";
 const selecionado = document.getElementsByClassName ('.selected');
+const abc = document.getElementsByClassName ('.feita');
 
 botaoAdd.addEventListener ('click', adicionar);
 botaoClearAll.addEventListener ('click', resetar);
 botaoClear.addEventListener ('click', rmvSel);
+abc.addEventListener ('click', cor);
 
 function adicionar () {
     if (tarefa.value !== "") {
@@ -42,4 +44,9 @@ function rmvSel () {
     while (selecionado.length > 0) {
         selecionado[0].parentNode.removeChild(selecionado[0]);
     }
+}
+
+function cor (event){
+    let ok = document.querySelector ('.feita');
+     event.target.style.background = getComputedStyle(ok).background = rgb (128, 128, 128);  
 }
