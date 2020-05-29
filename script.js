227a615e9ecef9f9fb2232d0df227d9b11555d3d
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-28 22:02:09 -0300

    Line-through

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ function addItem(){
   li.innerHTML = text;
   list.appendChild(li);
   textoTarefa.value = "";
-  li.addEventListener("click", changeColor)
+  li.addEventListener("click", changeColor);
+  li.addEventListener("dblclick", doubleClick);
 }
 
 function changeColor(event){
   let clickedItem = event.target;
   clickedItem.style.backgroundColor = "rgb(128, 128, 128)";
+}
+
+function doubleClick(event){
+  let clickedItem = event.target;
+  if (clickedItem.style.textDecoration !== "line-through"){
+    clickedItem.style.textDecoration = "line-through";
+  } else {
+    clickedItem.style.textDecoration = "";
+  }
 }
