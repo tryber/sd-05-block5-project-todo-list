227c4dd8effee768e9b6c5c627b86f1c179fe8cd
Lucas Staroscky <staroscky60@gmail.com>
2020-05-29 10:49:10 -0300

    reduce functionbackgroundColor and completedItem to have a clean code

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
-window.onload = function () {
+const elTexto = document.getElementById('texto-tarefa');
+const btnTarefa = document.getElementById('criar-tarefa');
+const btnClear = document.getElementById('apaga-tudo');
+const btnRemoveCompleted = document.getElementById('remover-finalizados');
+const btnRemoveSelected = document.getElementById("remover-selecionado")
+const listaTarefa = document.getElementById("lista-tarefas");
+const checkCompletedItem = document.querySelectorAll("li");
+const button = document.querySelectorAll("button")
 
- const buttonHTML = document.getElementsByTagName('button');
- const listHTML = document.getElementsByTagName('ol');
- const createTaskButton = document.getElementById("criar-tarefa");
- const inputValue = document.getElementById('texto-tarefa');
 
+// criar tarefa
+btnTarefa.addEventListener('click', () => {
+ const insertList = document.getElementsByTagName('ol')[0];
+ const valor = elTexto.value;
+ const tarefa = document.createElement('li');
+ tarefa.innerHTML = valor;
+ insertList.appendChild(tarefa);
+ elTexto.value = '';
+});
 
-
-
- // function to add the cursor pointer
- const cursorFunction = (event) => {
-  for (let i = 0; i < event.length; i += 1) {
-   let index = event[i];
-   index.style.cursor = "pointer";
-  }
+// alterar fundo pra cinza 
+listaTarefa.addEventListener("click", (event) => {
+ const item = event.target;
+ const isActive = item.classList.contains('active');
+ if (isActive === false) {
+  item.classList.add('active');
+ } else {
+  item.classList.remove('active');
  }
+});
 
- //  button cursor function 
- cursorFunction(buttonHTML)
-
- //  list cursor function
- cursorFunction(listHTML)
-
-
- // // trigger event to creake a task
- //  const orderList = document.querySelectorAll('ol')[0];
- //  const textValue = inputValue.value;
- //  let list = document.createElement('li');
- //  list.ondblclick = () => (list.style.backgroundColor = "grey")
- //  list.innerHTML = textValue;
- //  orderList.appendChild(list);
- //  inputValue.value = '';
-
-  
- 
-   createTaskButton.addEventListener("click", () => {
-    const list = document.createElement('li');
-    const orderList = document.querySelectorAll('ol')[0];
-    const textValue = inputValue.value;
-    list.innerHTML = textValue;
-    orderList.appendChild(list);
-    inputValue.value = '';
-    list.ondblclick = () => (list.style.backgroundColor = "grey")
-   })  
- 
-  
-
-
-
+// cursor pointer
+for (let i = 0; i < button.length; i += 1) {
+ button[i].style.cursor = 'pointer';
 }
+
+// riscar tarefa completa
+ const troughCompletedItems = (event) => {
+ const item = event.target;
+ const complete = item.classList.contains('completed');
+ if (complete === false) {
+  item.classList.add('completed');
+ } else {
+  item.classList.remove('completed');
+ }
+};
+
+
+// remover tarefas finalizadas
+btnRemoveCompleted.addEventListener("click", () => {
+ const list = document.querySelectorAll('.completed')
+ for (let i = 0; i < list.length; i += 1) {
+  list[i].parentNode.removeChild(list[i])
+ }
+});
+
+// limpar itens
+btnClear.addEventListener('click', () => {
+ const item = document.getElementsByTagName('ol');
+ for (let i = 0; i < item.length; i += 1) {
+  item[i].innerHTML = '';
+ }
+});
+
+// remover selecionado
+btnRemoveSelected.addEventListener("click", (event) => {
+ const a = document.querySelector(".active")
+ a.parentNode.removeChild(a)
+})
