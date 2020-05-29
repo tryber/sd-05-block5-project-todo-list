d504ccc04a936f29681ca053f389ccb890108b56
hleoc <informig@gmail.com>
2020-05-29 20:18:12 -0300

    Adicionando Itens na lista ordenada

---
+++ script.js
@@ -0,0 +1,11 @@
+const orderList = document.querySelector('#lista-tarefas');
+const botao = document.querySelector('#criar-tarefa');
+let arr = [];
+
+function adicionar (){
+    let lista = document.getElementById('texto-tarefa');
+    orderList.innerHTML = orderList.innerHTML + '<li>' + lista.value + '</li>';
+    arr = arr + lista.value + ' ';    
+}
+
+botao.addEventListener('click', adicionar);
