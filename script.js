f7b62d7ca0361a51debdeba6a4b1046b17b40e20
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-31 20:57:08 -0300

    botão apagar tudo está pronto.

--- script.js
+++ script.js
@@ -3,31 +3,39 @@ const criar = document.getElementById('criar-tarefa');
 const lista = document.getElementById('lista-tarefas');
 
 criar.addEventListener('click', function () {
-    const item = document.createElement('li');
-    item.innerHTML = input.value;
-    lista.appendChild(item);
-    input.value = '';
+  const item = document.createElement('li');
+  item.innerHTML = input.value;
+  lista.appendChild(item);
+  input.value = '';
 });
 
 lista.addEventListener('click', function () {
-    const evento = event.target;
-    evento.classList.add('corClique');
+  const evento = event.target;
+  evento.classList.add('corClique');
 });
 
-lista.addEventListener('dblclick', function() {
-    const evento = event.target;
-    let classes = evento.classList.value;
-    if (classes.search('completed') >= 0) {
-        evento.classList.remove('completed');
-    } else {
-        evento.classList.add('completed');
-    }
+lista.addEventListener('dblclick', function () {
+  const evento = event.target;
+  const classes = evento.classList.value;
+  if (classes.search('completed') >= 0) {
+    evento.classList.remove('completed');
+  } else {
+    evento.classList.add('completed');
+  }
 });
 
 const remover = document.getElementById('remover-finalizados');
 remover.addEventListener('click', function () {
-    let paraApagar = document.getElementsByClassName('completed');
-    for (let i = paraApagar.length; i > 0; i -= 1) {
-        lista.removeChild(paraApagar[0]);
-    }
+  const paraApagar = document.getElementsByClassName('completed');
+  for (let i = paraApagar.length; i > 0; i -= 1) {
+    lista.removeChild(paraApagar[0]);
+  }
+});
+
+const limpar = document.getElementById('apaga-tudo');
+limpar.addEventListener('click', function () {
+  const paraApagar = document.getElementsByTagName('li');
+  for (let i = paraApagar.length; i > 0; i -= 1) {
+    lista.removeChild(paraApagar[0]);
+  }
 });
