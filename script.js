1183c980d71efb17daa97ec26fd1b030083f8161
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-30 22:21:41 -0300

    o fundo do item da lista agora muda com dois cliques.

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ const lista = document.getElementById('lista-tarefas');
 criar.addEventListener('click', function () {
     const item = document.createElement('li');
     const span = document.createElement('span');
+    for (let i = -1; i < lista.childElementCount; i++) {
+        span.id = i + 1;
+    }
     span.innerHTML = input.value;
     item.appendChild(span);
     lista.appendChild(item);
     input.value = '';
+});
+
+lista.addEventListener('dblclick', function () {
+    const evento = event.target;
+    let id = evento.id;
+    id = Math.ceil(id);
+    console.log(typeof id === 'number');
+    if (typeof id === 'number') {
+        evento.classList.add('corClique');
+    }
 });
