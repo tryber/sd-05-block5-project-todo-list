cbcfd9397927d4600e2ab627839e981a8029958f
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-30 22:35:31 -0300

    tinha esquecido de nomear as classes de acordo com as intruções, e estava usando span dentro de li para fazer com que apenas o texto fosse marcado, mas mudei para o que o projeto pede.

--- script.js
+++ script.js
@@ -4,22 +4,17 @@ const lista = document.getElementById('lista-tarefas');
 
 criar.addEventListener('click', function () {
     const item = document.createElement('li');
-    const span = document.createElement('span');
-    for (let i = -1; i < lista.childElementCount; i++) {
-        span.id = i + 1;
-    }
-    span.innerHTML = input.value;
-    item.appendChild(span);
+    item.innerHTML = input.value;
     lista.appendChild(item);
     input.value = '';
 });
 
-lista.addEventListener('dblclick', function () {
+lista.addEventListener('click', function () {
     const evento = event.target;
-    let id = evento.id;
-    id = Math.ceil(id);
-    console.log(typeof id === 'number');
-    if (typeof id === 'number') {
-        evento.classList.add('corClique');
-    }
+    evento.classList.add('corClique');
+});
+
+lista.addEventListener('dblclick', function() {
+    const evento = event.target;
+    evento.classList.add('riscado');
 });
