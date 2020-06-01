b4f2cccb8222ae6a620e0eb5adf5abb26af82fbd
luialbeto <araujo.script@gmail.com>
2020-06-01 12:34:54 -0300

    line-through

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let criarTarefa = document.getElementById("criar-tarefa")
 let textoTarefa = document.getElementById("texto-tarefa")
 //clicar botão, novo item criado final lista e o texto do input deve ser limpo//
 
+criarTarefa.addEventListener("click", function() {
+  let perguntaUsuario = document.createElement("li");
+  perguntaUsuario.innerText = textoTarefa.value;
+  lista.appendChild(perguntaUsuario);
+  text.value = null;
+ })
+
+ listaTarefa.addEventListener("dblclick", function () {
+   if (selecionado.classList !== undefined) {
+       selecionado.classList.remove("selected")
+   }
+    event.target.classList.add("selected");
+      selecionado = event.target
+  }
