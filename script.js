7edbb7b1132d6711449aab8ba0759f1572c05b27
luialbeto <araujo.script@gmail.com>
2020-06-01 12:26:32 -0300

    estilo

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ let listaTarefa = document.getElementById("lista-tarefas")
 let criarTarefa = document.getElementById("criar-tarefa")
 let textoTarefa = document.getElementById("texto-tarefa")
 //clicar botão, novo item criado final lista e o texto do input deve ser limpo//
+
