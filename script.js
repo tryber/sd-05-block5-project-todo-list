79f123c5b65d54c557a78aa28457d11aaccdff6c
elisa <19753468+liscamillo@users.noreply.github.com>
2020-06-25 19:57:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,118 @@
-const criar_tarefa = document.getElementById('criar-tarefa');
-const salvar_tarefas = document.getElementById('salvar-tarefas');
-const remover_finalizados = document.getElementById('remover-finalizados');
-const remover_selecionado = document.getElementById('remover-selecionado');
-const apaga_tudo = document.getElementById('apaga-tudo');
-const mover_cima = document.getElementById('mover-cima');
-const mover_baixo = document.getElementById('mover-baixo')
-const input = document.getElementById('texto-tarefa') 
-const lista_tarefas = document.getElementById('lista-tarefas') 
+const criarTarefa = document.getElementById('criar-tarefa');
+const salvarTarefas = document.getElementById('salvar-tarefas');
+const removerFinalizados = document.getElementById('remover-finalizados');
+const removerSelecionado = document.getElementById('remover-selecionado');
+const apagaTudo = document.getElementById('apaga-tudo');
+const moverCima = document.getElementById('mover-cima');
+const moverBaixo = document.getElementById('mover-baixo');
+const input = document.getElementById('texto-tarefa');
+let listaTarefas = document.getElementById('lista-tarefas');
 
+// adds items
 
-// adds items to list
-
-criar_tarefa.addEventListener('click', adicionarItem)
-
-function adicionarItem(event){
+function adicionarItem(event) {
   event.preventDefault();
-  let novo_item = document.createElement('li');
-  let novo_item_texto = document.createTextNode(input.value)
-  novo_item.appendChild(novo_item_texto)
-  lista_tarefas.appendChild(novo_item)
+  const newItem = document.createElement('li');
+  const newItemText = document.createTextNode(input.value);
+  newItem.appendChild(newItemText);
+  listaTarefas.appendChild(newItem);
   input.value = '';
 }
+
+criarTarefa.addEventListener('click', adicionarItem);
 
 
 // toggles item state
 
-lista_tarefas.addEventListener('dblclick', toggleItemState);
-
 function toggleItemState(event) {
-  let selected = event.target;
+  const selected = event.target;
 
   // adds 'completed' class
   if (selected.classList.contains('completed')) {
     selected.classList.remove('completed');
   } else {
-      selected.classList.add('completed');
-    }
+    selected.classList.add('completed');
+  }
 
   // styles selected item with strikethrough
-  if (selected.style.textDecoration == 'line-through') {
-    selected.style.removeProperty('text-decoration')
+  if (selected.style.textDecoration === 'line-through') {
+    selected.style.removeProperty('text-decoration');
   } else {
     selected.style.textDecoration = 'line-through';
   }
 }
 
+listaTarefas.addEventListener('dblclick', toggleItemState);
+
 
 // adds id to selected item and remove from previous selected
 
-lista_tarefas.addEventListener('click', selectItem)
-
 function selectItem(event) {
-  let selected = event.target;
-  let items_lista = document.querySelectorAll('#lista-tarefas li');
-  for (let i = 0; i < items_lista.length; i++) {
-    items_lista[i].removeAttribute('id')
+  const selected = event.target;
+  const listItems = document.querySelectorAll('#lista-tarefas li');
+  for (let i = 0; i < listItems.length; i += 1) {
+    listItems[i].removeAttribute('id');
   }
-  selected.setAttribute('id', 'selected')
+  selected.setAttribute('id', 'selected');
 }
 
-
-// removes completed tasks
-
-remover_finalizados.addEventListener('click', removesCompleted);
-
-function removesCompleted(){
-  let items_lista = document.querySelectorAll('#lista-tarefas li');
-  for (let i = 0; i < items_lista.length; i++) {
-    if(items_lista[i].classList.contains('completed')){
-      items_lista[i].remove();
-    }
-  }
-}
-
-
-// removes selected item
-
-remover_selecionado.addEventListener('click', removeSelected)
-
-function removeSelected() {
-  let selected = document.getElementById('selected');
-  console.log(selected)
-  selected.remove();
-}
-
-
-// clears list
-
-apaga_tudo.addEventListener('click', clearAll);
-
-function clearAll(){
-  let items_lista = document.querySelectorAll('#lista-tarefas li');
-  for (let i = 0; i < items_lista.length; i++) {
-    items_lista[i].remove();
-  }
-}
+listaTarefas.addEventListener('click', selectItem);
 
 
 // changes background of selected item
 
-lista_tarefas.addEventListener('click', changeBg);
+listaTarefas.addEventListener('click', changeBg);
 
-function changeBg(event){
-  let target = event.target;
+function changeBg(event) {
   let selected = event.target;
   selected.style.backgroundColor = 'rgb(128,128,128)';
 }
 
 
-// moves item up and down
+// removes completed tasks
 
-mover_cima.addEventListener('click', moveTop)
-
-function moveTop() {
-  let selected = document.getElementById('selected');
-  lista_tarefas.insertBefore(selected, selected.previousSibling)
+function removesCompleted() {
+  const listItems = document.querySelectorAll('#lista-tarefas li');
+  for (let i = 0; i < listItems.length; i += 1) {
+    if (listItems[i].classList.contains('completed')) {
+      listItems[i].remove();
+    }
+  }
 }
 
-mover_baixo.addEventListener('click', moveDown)
+removerFinalizados.addEventListener('click', removesCompleted);
+
+
+// removes selected item
+
+function removeSelected() {
+  const selected = document.getElementById('selected');
+  selected.remove();
+}
+
+removerSelecionado.addEventListener('click', removeSelected);
+
+
+// clears list
+
+function clearAll() {
+  const listItems = document.querySelectorAll('#lista-tarefas li');
+  for (let i = 0; i < listItems.length; i += 1) {
+    listItems[i].remove();
+  }
+}
+
+apagaTudo.addEventListener('click', clearAll);
+
+
+// moves item up and down
+
+function moveTop() {
+  const selected = document.getElementById('selected');
+  listaTarefas.insertBefore(selected, selected.previousSibling);
+}
+
+moverCima.addEventListener('click', moveTop);
 
 function moveDown() {
   // there is not insertAfter function, so we have to do this:
@@ -125,14 +120,16 @@ function moveDown() {
   // selected = 1; nextElement = 2; then nextElement of 2 = 3
   // insert selected before 3
   // 2 > 1 > 3
-  let selected = document.getElementById('selected');
+  const selected = document.getElementById('selected');
   let nextElement = selected.nextSibling;
   nextElement = nextElement.nextSibling;
-  lista_tarefas.insertBefore(selected, nextElement)
+  listaTarefas.insertBefore(selected, nextElement);
 }
 
+moverBaixo.addEventListener('click', moveDown);
 
-// saves list on local storage
+
+// saves list
 
 function SavesList(){
   localStorage.setItem('list', listaTarefas.innerHTML);
