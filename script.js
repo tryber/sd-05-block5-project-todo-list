5f8ab1c77f130222511dae8e3df6f372d5d059de
Isabela Rosa <isabelarosa954@gmail.com>
2020-07-07 05:12:13 -0300

    adicionando o cursor pointer aos elementos da lista e botões

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function criaTarefa() {
   document.getElementById('lista-tarefas').appendChild(novaTarefa);
   // criando funcionalidade que limpa input após o click:
   document.getElementById('texto-tarefa').value = '';
+  // fazendo com que cada novo item da lista receba a maozinha com o passar do mouse:
+  novaTarefa.style.cursor = 'pointer';
 }
-
 // adicionando o evento de adição de tarefa ao botão:
 const botaoCriarTarefa = document.getElementById('criar-tarefa');
 botaoCriarTarefa.addEventListener('click', criaTarefa);
