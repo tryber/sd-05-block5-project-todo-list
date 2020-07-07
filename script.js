ec54b3caa60db73c57db741bc9f7ce1cbe5e7bd7
Isabela Rosa <isabelarosa954@gmail.com>
2020-07-07 03:52:53 -0300

    corrigindo issues codeclimate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // criando função que incluirá uma tarefa à lista:
-function criaTarefa () {
-  let novaTarefa = document.createElement('li');
+function criaTarefa() {
+  const novaTarefa = document.createElement('li');
   novaTarefa.innerHTML = document.getElementById('texto-tarefa').value;
   document.getElementById('lista-tarefas').appendChild(novaTarefa);
   // criando funcionalidade que limpa input após o click:
@@ -8,5 +8,5 @@ function criaTarefa () {
 }
 
 // adicionando o evento de adição de tarefa ao botão:
-let botaoCriarTarefa = document.getElementById('criar-tarefa');
+const botaoCriarTarefa = document.getElementById('criar-tarefa');
 botaoCriarTarefa.addEventListener('click', criaTarefa);
