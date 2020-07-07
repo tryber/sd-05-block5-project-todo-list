1a059b333bdbb47f36aec33721cb8433a6416e26
Isabela Rosa <isabelarosa954@gmail.com>
2020-07-07 05:35:48 -0300

    alterando cor de fundo do item ao ser clicado

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ function criaTarefa() {
   document.getElementById('texto-tarefa').value = '';
   // fazendo com que cada novo item da lista receba a maozinha com o passar do mouse:
   novaTarefa.style.cursor = 'pointer';
+  // mudando a cor do item da lista ao ser clicado:
+  novaTarefa.addEventListener('click', function () {
+    novaTarefa.style.backgroundColor = 'rgb(128,128,128)';
+  })
 }
 // adicionando o evento de adição de tarefa ao botão:
 const botaoCriarTarefa = document.getElementById('criar-tarefa');
