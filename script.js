24af2e73e11906ba03fa4963dd578299ef766173
Isabela Rosa <isabelarosa954@gmail.com>
2020-07-07 22:31:18 -0300

    refatorando o código - arrumando a bagunça

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+// declarando as variaveis e botoes:
+let textoInput = document.getElementById('texto-tarefa');
+let botaoCriarTarefa = document.getElementById('criar-tarefa');
+let listaTarefas = document.getElementById('lista-tarefas');
+let botaoApagaTudo = document.getElementById('apaga-tudo');
+let removerFinalizados = document.getElementById('remover-finalizados');
+// criando o item li:
+// esta função adiciona o itemLi como filho da lista de tarefas
+// e limpa a entrada de texto
+function criaItem () {
+  let itemLi = document.createElement('li');
+  itemLi.innerHTML = textoInput.value;
+  listaTarefas.appendChild(itemLi);
+  textoInput.value = '';
+}
+// evento que cria tarefa:
+botaoCriarTarefa.addEventListener('click', criaItem);
+
+
+
+
+/*
 // criando função que incluirá uma tarefa à lista:
 function criaTarefa() {
+  // criando o elemento li:
   const novaTarefa = document.createElement('li');
+  // adicionando ao li o texto do input:
   novaTarefa.innerHTML = document.getElementById('texto-tarefa').value;
+  // adicionando o elemento li como filho da ol:
   document.getElementById('lista-tarefas').appendChild(novaTarefa);
-  //localStorage.setItem('tarefa', )
-  // criando funcionalidade que limpa input após o click:
+  // localStorage.setItem('tarefa', )
+  // funcionalidade que limpa input após o click:
   document.getElementById('texto-tarefa').value = '';
   // fazendo com que cada novo item da lista receba a maozinha com o passar do mouse:
   novaTarefa.style.cursor = 'pointer';
@@ -13,23 +38,22 @@ function criaTarefa() {
     novaTarefa.style.backgroundColor = 'rgb(128,128,128)';
   });
   // riscar o item com clique duplo:
-  novaTarefa.addEventListener('dblclick', function () {
+  novaTarefa.addEventListener('dblclick', riscaLi);
+  function riscaLi () {
     novaTarefa.className = 'completed';
     novaTarefa.style.textDecoration = 'line-through';
-    // removendo classe completed e risco no item:
-    novaTarefa.addEventListener('dblclick', function () {
-      if (novaTarefa.className === 'completed') {
-        novaTarefa.classList.remove('completed');
-        novaTarefa.style.textDecoration = 'none';
-        novaTarefa.style.backgroundColor = 'aliceblue';
-      }
-      else {
-        novaTarefa.className = 'completed';
-      }
-    });
-  });
+  }
+  // removendo classe completed e risco no item clicado novamente:
+  novaTarefa.addEventListener('dblclick', removeRisco);
+  function  removeRisco () {
+    if (novaTarefa.className === 'completed') {
+      novaTarefa.classList.remove('completed');
+      novaTarefa.style.textDecoration = 'none';
+      novaTarefa.style.backgroundColor = 'aliceblue';
+    }
+  }
 }
-// adicionando o evento de adição de tarefa ao botão:
+// colocando o evento de adição de tarefa ao botão:
 const botaoCriarTarefa = document.getElementById('criar-tarefa');
 botaoCriarTarefa.addEventListener('click', criaTarefa);
 // criando função que remove todos os elementos li:
@@ -40,14 +64,21 @@ function apagaTudo() {
     lista.firstChild.remove();
   }
 }
-// adicionando o evento de apagar todos os elementos ao botão:
+// colocando o evento de apagar todos os elementos ao botão:
 botaoApagaTudo.addEventListener('click', apagaTudo);
-// apagando somente os elementos finalizados:
+// função que apague somente os elementos finalizados:
 const botaoApagaFinalizados = document.getElementById('remover-finalizados');
 function apagaFinalizados() {
+  const tarefaFinalizada = document.querySelector('li.completed');
   document.querySelector('li.completed').remove();
-  /* if (document.querySelector('li.completed') = null) {
-    document.querySelector(null).remove();
-  } */
+  // apagar elemento que já perdeu a classe completed:
+  if (document.querySelector('li').className !== 'completed') {
+    document.querySelector('li').className = 'completed';
+    document.querySelector('li.completed').remove();
+    //document.querySelector(null).remove();
+  }
 }
 botaoApagaFinalizados.addEventListener('click', apagaFinalizados);
+
+// COLOCAR UM IF/ELSE DENTRO DA FUNÇÃO QUE APAGA OS ITENS FINALIZADOS E NÃO NA FUNÇÃO QUE REMOVE A CLASSE COMPLETED
+*/
